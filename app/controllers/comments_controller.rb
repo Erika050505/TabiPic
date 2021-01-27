76b9cd25d5557d6a0c827234d174fd0016c4bb38
EC2 Default User <ec2-user@ip-172-31-47-159.ap-northeast-1.compute.internal>
2021-01-27 07:56:44 +0000

    コメントバリデーション追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,8 +3,10 @@ class CommentsController < ApplicationController
     post = Post.find(params[:post_id])
     comment = current_customer.comments.new(comment_params)
     comment.post_id = post.id
-    comment.save
-    redirect_to post_path(params[:post_id])
+    if comment.save
+      redirect_to post_path(params[:post_id])
+    else
+      render 'posts/show'
   end
 
   def destroy
